5a1e19b9de4c79ea5fae15e8f27c707e932824a8
mac <mac@blockapi.dev>
2024-04-25 00:00:00 +0800

    up

--- cmd/hati/main.go
+++ cmd/hati/main.go
@@ -2,12 +2,16 @@
 package main
 
 import (
+	"runtime"
+
 	"github.com/hati-sh/hati/cmd/hati/commands"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	_ = godotenv.Load()
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	commands.Execute()
 }
