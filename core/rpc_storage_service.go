dc84d15d75348f3bcbe29f249426684c2705c2dc
mac <mac@blockapi.dev>
2024-04-27 13:26:04 +0800

    rpc method to set and get

--- core/rpc_storage_service.go
+++ core/rpc_storage_service.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"errors"
+
 	"github.com/hati-sh/hati/storage"
 )
 
@@ -8,12 +10,55 @@ type RpcStorageService struct {
 	storage *storage.Storage
 }
 
-type Args struct{}
+type CountArgs struct{}
 
-func (s *RpcStorageService) Count(args *Args, reply *int) error {
+type SetArgs struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type GetArgs struct {
+	Key string `json:"key"`
+}
+
+func (s *RpcStorageService) Count(args *CountArgs, reply *int) error {
 	// Fill reply pointer to send the data back
 	count, _ := s.storage.Count(storage.Memory)
 	*reply = count
+
+	return nil
+}
+
+func (s *RpcStorageService) Set(args *SetArgs, reply *bool) error {
+	if args.Key == "" {
+		return errors.New("invalid key")
+	}
+
+	// Fill reply pointer to send the data back
+	if err := s.storage.Set(storage.Memory, []byte(args.Key), []byte(args.Value)); err != nil {
+		*reply = false
+
+		return nil
+	}
+
+	*reply = true
+
+	return nil
+}
+
+func (s *RpcStorageService) Get(args *SetArgs, reply *string) error {
+	if args.Key == "" {
+		return errors.New("invalid key")
+	}
+
+	value, err := s.storage.Get(storage.Memory, []byte(args.Key))
+	if err != nil {
+		*reply = err.Error()
+
+		return nil
+	}
+
+	*reply = string(value)
 
 	return nil
 }
